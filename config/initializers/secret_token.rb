6e5d3a4d0ad510cd32df6e36e7183f3de1d35554
Sugino Yasuhiro <suginoyasuhiro@gmail.com>
2011-12-04 11:31:48 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yontaku::Application.config.secret_token = 'b532ed34917d9ded6e9de7fbbd7e2c3df4b78a5ac6ada249937f6b7ee1f4ecbb6999ddb1861a740902b58b8775fab03928bfe370ce5df21fcc055d9bc5dc6d1b'
