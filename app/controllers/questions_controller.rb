13d92f1615b7331dbc7339935c0ba2e6368f1478
marutanm <marutanm@gmail.com>
2011-12-04 14:27:59 +0900

    htmlをlayoutに分割

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,7 +13,7 @@ class QuestionsController < ApplicationController
   # GET /questions/1
   # GET /questions/1.json
   def show
-    @question = Question.find(params[:id])
+    # @question = Question.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
