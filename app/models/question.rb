73532b825e6d317598cd8b0245af723bb42bc76e
Sugino Yasuhiro <suginoyasuhiro@gmail.com>
2011-12-07 20:37:09 +0900

    不要な行をnamed scopeを削除

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,6 @@
 class Question < ActiveRecord::Base
   default_scope order("id")
   scope :other_questions, lambda { |previous_id| where("id != ?", previous_id) }
-  scope :other, lambda { |previous_id| where("id != ?", previous_id) }
   def self.other_question(id)
     self.other_questions(id).sample
   end 
